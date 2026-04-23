f0354ed978c8a1a413d259e959a78b64c156bca8
santifont <sanalffer@alu.edu.gva.es>
2026-04-23 08:39:36 +0200

    Movimiento del jugador incorporado

---
+++ Assets/Scripts/MovimientoJugador.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    CharacterController controller;
+    [SerializeField] private float velocidad = 7.5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = this.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        //Capturo el movimiento en los ejes
+        float movimientoH = Input.GetAxis("Horizontal");
+        float movimientoV = Input.GetAxis("Vertical");
+
+        Vector3 anguloTeclas = new Vector3(movimientoH, 0f, movimientoV);
+
+        transform.Translate(anguloTeclas * velocidad * Time.deltaTime, Space.World);
+
+        //Genero el vector de movimiento
+        //Muevo el jugador
+        //transform.position += anguloTeclas * velocidad * Time.deltaTime;
+        controller.Move(anguloTeclas * velocidad * Time.deltaTime);
+        if (anguloTeclas != null && anguloTeclas != Vector3.zero)
+        {
+            transform.forward = anguloTeclas * 1;
+            transform.rotation = Quaternion.LookRotation(anguloTeclas);
+        }
+    }
+}
