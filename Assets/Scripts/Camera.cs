447e1ba38d027bbc02a6e72c77372a9022b641a8
santifont <sanalffer@alu.edu.gva.es>
2026-04-29 08:51:05 +0200

    Camera settings
    Ajustes de la cámara.

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -2,16 +2,29 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    private Transform jugador;
+    private GameObject jugador;
+    private Vector3 difference;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        jugador = GameObject.Find("Jugador").GetComponent<Transform>();
+        jugador = GameObject.Find("Jugador");
+        difference = jugador.transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(jugador.position);
+        transform.position = new Vector3(
+            jugador.transform.position.x, 20,
+            (jugador.transform.position.z - 10));
+
+        if (jugador.transform.position.z < -24)
+        {
+            transform.position = new Vector3(
+            jugador.transform.position.x, 20, -34);
+
+            transform.LookAt(jugador.transform);
+        }
     }
 }
